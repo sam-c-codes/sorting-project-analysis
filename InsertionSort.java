f27fcda4a2b5688ed7db8c6e44975f9784c656e3
Simran Chawla <sc1236@pleasantonusd.net>
2025-01-24 22:01:16 +0000

    sam

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,5 +2,12 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+        for (int i = 1; i < input.length; i++) {
+            int temp = input[i];
+            for (int k = i - 1; k >= 0 && temp < input[i]; k--) {
+                input[k+1] = input[k];
+            input[k] = temp;
+            }
+        }
     }
 }
